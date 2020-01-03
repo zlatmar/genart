ed6563e076b8486d4b537ea2826053e82372d2c6
Zlatoslav <z.marchev@abv.bg>
2020-01-03 10:21:44 +0200

    Random colored circles

--- sketch.js
+++ sketch.js
@@ -1,21 +1,27 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
-
+const paletts = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(paletts).slice(0, colorCount));
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 30;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          color: random.pick(palette),
+          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          position: [u, v]
+        });
       }
     }
     return points;
@@ -29,15 +35,25 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+
+      const {
+        position,
+        radius,
+        color
+      } = data;
+
+      const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       context.strokeStyle = 'black';
-      context.lineWidth = 40;
-      context.stroke();
+      context.lineWidth = 20;
+      context.fillStyle = color;
+      context.fill();
     });
   };
 };
