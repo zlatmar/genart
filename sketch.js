1795b8b5085434adc94f10e544491a0e381dac8e
Zlatoslav <z.marchev@abv.bg>
2020-01-03 10:30:28 +0200

    Add noise

--- sketch.js
+++ sketch.js
@@ -17,9 +17,11 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          radius,
           position: [u, v]
         });
       }
@@ -27,7 +29,6 @@ const sketch = () => {
     return points;
   }
 
-  random.setSeed(512);
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
